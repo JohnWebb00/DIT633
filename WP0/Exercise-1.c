fa958138ca53e9ed2076c5ee55f8d8f163588725
johnchri <johnchri@student.chalmers.se>
2023-01-18 13:05:41 +0100

    Add WP0 exercise solutions

---
+++ WP0/Exercise-1.c
@@ -0,0 +1,10 @@
+// (C) John Webb, Carl Dahlqvist, group 20 (2023)
+// Work package 0
+// Exercise 1
+// Submission Code 0
+#include <stdio.h>
+
+int main (void) {
+        printf("%s", "Hello World!\n");
+    }
+    
