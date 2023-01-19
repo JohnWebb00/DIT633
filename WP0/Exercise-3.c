310e29a31776676cde463f6aa2755ce32fe46b4d
johnchri <johnchri@student.chalmers.se>
2023-01-19 08:50:31 +0100

    Add comments

--- WP0/Exercise-3.c
+++ WP0/Exercise-3.c
@@ -6,11 +6,13 @@
 #include <string.h>
 
 int main (int argc, char *argv[]) {
-    char string[] = "-h";
+    // If the argument count is less than 2 or greater than two, the user has either not included an argument or has provided too many arguments.
   if(argc < 2 || argc > 2){
       printf("%s", "Error: must enter one program argument. To learn more, use argument '-h'\n");
-  } else if (strcmp(argv[1], string) == 0){
+    // Compares the argument string with "-h"
+  } else if (strcmp(argv[1], "-h") == 0){
       printf("%s", "Enter an argument after the program name.\n e.g ./program argument\n");
+    // If argc is 2, user has included one argument.
   } else if (argc == 2){
         printf("Hello World! - I'm %s!\n", argv[1]);
     }
